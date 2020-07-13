23e4d9f45389d14113a4cda13a286258a56a69e3
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-07-13 17:01:58 +0200

    bored-api

---
+++ svelte-boredapi/src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Activity from './components/Activity.svelte'
+	let activity = {}
+	let show = false
+	//let type = 'recreational'
+	const getActivity = () =>{
+		show = false
+		fetch('https://www.boredapi.com/api/activity?type=recreational')
+			.then( res => res.json() )
+				.then( json => activity = json )
+				.then( json => {
+					activity = json
+					show = true
+				})
+		}
+
+
+</script>
+
+<main>
+	<button on:click={getActivity}>Hit Me</button>
+	<!--input type="type" bind:value={type}-->
+	{#if show}
+		<!--Content here-->
+		<Activity {activity} />
+	{:else}
+		<!--Content here-->
+		<h2>Show Content here</h2>
+	{/if}
+	<!--Activity activity={activity}/-->
+</main>
+
+<style>
+
+	main{
+		display: grid;
+		place-items: center;
+		height: 100vh;
+		position: relative;
+	}
+	button{
+		position: absolute;
+		top: 2rem;
+		background: rgb(32, 44, 48);
+		color: #eee;
+		cursor: pointer;
+	}
+
+</style>
