b9858d7abaaf8a86a458bad2576240f6135ae2e2
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-07-13 17:03:07 +0200

    datamuse

---
+++ svelte-datamuse/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Datamuse from './components/Datamuse.svelte'
+	let words = ''
+</script>
+
+<main>
+	<div class="input">
+	<input type="text" bind:value={words}>
+	</div>
+	<Datamuse {words}/>
+</main>
+
+<style>
+
+    main{
+        display: grid;
+		grid-template-rows: 10vh 90vh;
+    }
+
+	.input{
+		display: grid;
+		place-items: center;
+	}
+
+	input{
+		width: 40vw;
+		font-size: 1.2rem;
+		padding: .8rem;
+		outline: none;
+		border-radius: 1rem;
+	}
+
+	
+
+</style>
